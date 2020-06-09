16ff49f5877d7d66cd9a4ff15cc8169e403f9d76
code-loading <eahmed711@gmail.com>
2020-06-09 15:34:35 +0400

    some changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function MediaCard({ title, body, imageURL, alt1, body2 }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{body2}</p>
+      <p>{body}</p>
+      <img src={imageURL} alt={alt1} />
+    </div>
+  );
+}
+
+function Gate({ isOpen }) {
+  return (
+    <div>
+      <p>This is a separate function called Gate: </p>
+      Gate is {isOpen ? "open" : "closed"}
+      <br />
+      <p>This is calculated inside a js tag: {5 + 10}</p>
+      
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
+  /* <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode> */
+  <div>
+    <MediaCard
+      title="This MediaCard will solve all your business problems!"
+      body2="Doing the react tutorial by dave"
+      body=<strong>"We are using revolutionary technology"</strong>
+      imageURL="https://www.pethealthnetwork.com/sites/default/files/cat-diarrhea-when-is-it-serious-and-how-do-i-stop-it-483371318.jpg"
+      alt1="a cat"
+    />
+    <Gate isOpen={false} />
+  </div>,
   document.getElementById('root')
 );
 
